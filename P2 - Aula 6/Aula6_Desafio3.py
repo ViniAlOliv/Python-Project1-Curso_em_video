ea216f9f24215ca5dc1f53029f85013ad5c7748a
ViniAlOliv <viniciusaloli@gmail.com>
2023-03-22 22:35:59 -0300

    Atividade 6.3

--- P2 - Aula 6/Aula6_Desafio3.py
+++ P2 - Aula 6/Aula6_Desafio3.py
@@ -3,3 +3,21 @@ n1 = int(input('Digite um valor:'))
 n2 = int(input('Digite outro valor:'))
 soma = n1 + n2
 print('A soma entre {0} e {1} é: {2}'.format(n1, n2, soma))
+
+print('')
+print('')
+#Crie outras coisas que faça leitura...
+anoatual = int(input('Ano atual: '))
+idade = int(input('Sua idade atual: '))
+anoNasc = anoatual - idade
+print('O ano atual é {0} e idade {1}, então nasceu no ano de {2}'.format(anoatual, idade, anoNasc))
+
+
+#Conta de restaurante
+qtPessoas = int(input('Quantidade de pessoas que foram jantar:'))
+valorConta = float(input('Valor da conta: '))
+valor = valorConta / qtPessoas
+print('A conta deu R${0}. Para {1} pessoa/s ficou dividido em R${2} para cada pessoa'.format(valorConta, qtPessoas, valor))
+
+
+#
--- P2 - Aula 6/Aula6_Desafio4.py
+++ P2 - Aula 6/Aula6_Desafio4.py
@@ -1,2 +1,5 @@
 #Faça um programa que leia algo pelo teclado e mostre na tela
 # o seu tipo e todas as informações possíveis sobre ele:
+
+#Experimentar todos os IS...
+
