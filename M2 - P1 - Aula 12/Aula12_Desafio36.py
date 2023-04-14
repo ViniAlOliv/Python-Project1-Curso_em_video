c388cab401b3e2a5c41372fed763282532b391a2
ViniAlOliv <viniciusaloli@gmail.com>
2023-04-14 00:08:04 -0300

    Aula 12 - Realizando atividades

--- M2 - P1 - Aula 12/Aula12_Desafio36.py
+++ M2 - P1 - Aula 12/Aula12_Desafio36.py
@@ -5,3 +5,20 @@
 
 #Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado
 
+print('Olá, tudo bem? Você quer comprar uma casa conosco:')
+casaValor = float(input('Qual é o valor da casa: R$'))
+salarioComprador = float(input('Quanto o comprador ganha por mês: R$'))
+anosAPagar = int(input('Durante quantos anos irá pagar: '))
+
+prestacaoMensal = casaValor / (anosAPagar * 12)
+print('Prestação Mensal: R$ {:.2f}'.format(prestacaoMensal))
+
+parteSalario = salarioComprador * 0.3
+print('30 por cento do salário R${} é R${}'.format(salarioComprador, parteSalario))
+
+if (prestacaoMensal > parteSalario):
+    print('Infelizmente o empréstimo será negado.')
+#elif (prestacaoMensal <= parteSalario):
+#    print('Você pode adquirir o empréstimo. Parabéns!')
+else:
+    print('Você pode adquirir o empréstimo. Parabéns!')
--- M2 - P1 - Aula 12/Aula12_Desafio37.py
+++ M2 - P1 - Aula 12/Aula12_Desafio37.py
@@ -3,3 +3,15 @@
 #2 para Octal
 #3 para hexadecimal
 
+numero = int(input('Escreva um número inteiro: '))
+print('Selecione um conversor: \n [1] Binário\n [2] Octal\n [3] Hexadecimal')
+conversao = int(input('Escolha um dos números: '))
+
+if (conversao == 1):
+    print('O valor {} convertido para binário é igual a {}'.format(numero, format(numero, "b")))
+elif (conversao == 2):
+    print('O valor {} convertido para octal é igual a {}'.format(numero, format(numero, "o")))
+elif (conversao == 3):
+    print('O valor {} convertido para hexadecimal é igual a {}'.format(numero, format(numero, "x")))
+else:
+    print('O devido valor incluso não foi encontrado. Tente novamente')
