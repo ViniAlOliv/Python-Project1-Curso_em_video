0e41c616f2109aa57a97ffa47a08ad68b01880e5
ViniAlOliv <viniciusaloli@gmail.com>
2023-04-18 00:08:28 -0300

    Aula 13 - Ainda na aula. Pausa para sono

---
+++ M2 - P2 - Aula 13/Aula13.py
@@ -0,0 +1,24 @@
+#Aula 13 - Estrutura de Repetição FOR
+'''
+for c in range(1,10):
+	passo
+pega
+'''
+#Aula prática
+#Oi
+for c in range(1, 6):
+    print('Oi')
+print('FIM')
+print('-='*20)
+
+#Contar para trás
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
+print('-='*20)
+
+#Pular de dois em dois
+for c in range(0, 7, 2):
+    print(c)
+print('FIM')
+print('-='*20)
